75969c00492f3d48075df68028388a54f00223af
RockSands <117019049@qq.com>
2017-12-08 09:40:20 +0800

    补充提交

--- src/main/java/com/kettle/core/instance/metas/KettleDBMeta.java
+++ src/main/java/com/kettle/core/instance/metas/KettleDBMeta.java
@@ -1,7 +1,5 @@
 package com.kettle.core.instance.metas;
 
-import com.kettle.core.instance.KettleMgrEnvironment;
-
 /**
  * 数据库元数据
  * 
@@ -39,21 +37,6 @@ public class KettleDBMeta {
 	 * 数据库登入密码
 	 */
 	private String passwd;
-
-	/**
-	 * 是否使用连接池
-	 */
-	private boolean usePool;
-
-	/**
-	 * 连接池初始大小
-	 */
-	private Integer poolInit;
-
-	/**
-	 * 连接池最大值
-	 */
-	private Integer poolMax;
 
 	public String getType() {
 		return type;
@@ -101,33 +84,5 @@ public class KettleDBMeta {
 
 	public void setPasswd(String passwd) {
 		this.passwd = passwd;
-	}
-
-	public boolean isUsePool() {
-		return usePool;
-	}
-
-	public void setUsePool(boolean usePool) {
-		if (usePool) {//初始化的时候将默认值设置
-			setPoolMax(poolMax == null ? KettleMgrEnvironment.KETTLE_RECORD_DB_POOL_MAX : poolMax);
-			setPoolInit(poolInit == null ? KettleMgrEnvironment.KETTLE_RECORD_DB_POOL_INIT : poolInit);
-		}
-		this.usePool = usePool;
-	}
-
-	public Integer getPoolInit() {
-		return poolInit;
-	}
-
-	public void setPoolInit(Integer poolInit) {
-		this.poolInit = poolInit;
-	}
-
-	public Integer getPoolMax() {
-		return poolMax;
-	}
-
-	public void setPoolMax(Integer poolMax) {
-		this.poolMax = poolMax;
 	}
 }
